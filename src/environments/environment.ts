d7a217c6454d03878e3bc2983c0ed5d506793d53
kei <107705214+ksk3gogodayo@users.noreply.github.com>
2026-03-16 07:19:08 +0900

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
+    apiKey: 'AIzaSyDpBT8h6k8esF-96FsIGtwZ5oSi4lOG5C0',
+    authDomain: 'kakeibo-app-54921.firebaseapp.com',
+    projectId: 'kakeibo-app-54921',
+    storageBucket: 'kakeibo-app-54921.firebasestorage.app',
+    messagingSenderId: '604104453174',
+    appId: '1:604104453174:web:edfb5be76d16afa647fc97'
   }
 };
